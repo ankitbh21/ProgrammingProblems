a4d6bfb2352de4d1e8675c3eb303e6a40423c8c7
Ankit Bhardwaj <ankitbh21@gmail.com>
2016-05-06 15:30:46 +0530

    KMP Substring Algorithm

--- Suffix-Prefix/KMPSubStringSearch.cpp
+++ Suffix-Prefix/KMPSubStringSearch.cpp
@@ -16,21 +16,74 @@ using namespace std;
 
 int * createSuffixArray(string text)
 {
-	int arr[text.length()];
-	arr[0] = 5;
-	arr[1] = 3;
+	int* arr = (int*) malloc (text.length());
+	*(arr) = 0;
+	int j = 0;
+	int i = 1;
+	for(int itr = 1; itr < text.length();)
+	{
+		if(text[i] == text[j])
+		{
+			*(arr + itr) = j + 1;
+			i++;
+			j++;
+			itr++;
+		}
+		else
+		{
+			if(j > 0)
+			{
+				j = arr[j-1];
+			}
+			else
+			{
+				*(arr + itr) = 0;
+				i++;
+				itr++;
+			}
+		}
+	}
 	return arr;
 }
 
 int patternMatch(string text, string pattern)
 {
+	int* arr = createSuffixArray("abcabe");
+	int textIndex = 0;
+	int patternIndex = 0;
+	int patternLen = pattern.length();
+	while(textIndex < text.length() && patternIndex < patternLen)
+	{
+		if(text[textIndex] == pattern[patternIndex])
+		{
+			textIndex++;
+			patternIndex++;
+			if(patternIndex == patternLen)
+			{
+				return textIndex - patternLen;
+			}
+		}
+		else
+		{
+			if(patternIndex == 0)
+			{
+				textIndex++;
+			}
+			else
+			{
+				patternIndex = arr[patternIndex - 1];
+			}
+		}
+	}
 	return -1;
 }
 
 int main()
-{
-	int * a = createSuffixArray("hi");
-	cout<<a[0]<<" "<<a[1];
-	cout<<patternMatch("ankitbhardwaj", "bhard");
-	return -1;
+{	
+	cout<<patternMatch("ankitbhardwaj","bhard")<<endl;
+	cout<<patternMatch("ankitbhardwaj","hard")<<endl;
+	cout<<patternMatch("ankitbhardwaj","ahard")<<endl;
+	cout<<patternMatch("ankitbhardwaj","hardwaj")<<endl;
+	cout<<patternMatch("ankitbhardwaj","ank")<<endl;
+	return 0;
 }
